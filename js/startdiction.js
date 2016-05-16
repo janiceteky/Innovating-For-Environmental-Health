625550e2b63098f65e5d72fcb501af8c09f8557b
Janice M. Sheffield <janiceteky@yahoo.com>
2016-05-16 10:43:28 -0400

    First Commit

---
+++ js/startdiction.js
@@ -0,0 +1,22 @@
+function startDictation() {
+	if (window.hasOwnProperty('webkitSpeechRecognition')) {
+		var recognition = new webkitSpeechRecognition();
+		recognition.continuous = false;
+		recognition.interimResults = false;
+
+		recognition.lang = "it-IT";
+
+		recognition.start();
+
+		recognition.onresult = function(e) {
+
+			document.getElementById('transcript').value = e.results[0][0].transcript;
+			recognition.stop();
+			document.getElementById('labnol').submit();
+		};
+
+		recognition.onerror = function(e) {
+			recognition.stop();
+		}
+	} //END IF
+}
